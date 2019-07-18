66ffd768ccb6aa69728564b079caebc8c3710e22
Ainea Wabwoba <aineah28@gmail.com>
2019-07-18 22:32:39 +0300

    Initial commit

---
+++ buildScripts/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+const port = 3000;
+
+
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err)=>{
+  (err) ? console.log(err): open(`http://localhost: ${port}`);
+});
